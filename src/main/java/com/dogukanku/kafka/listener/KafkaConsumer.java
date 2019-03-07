835b12056be505dc9cc3b4abaf7a2fd8d385e84f
Dogukan Kundum <dogukan.kundum@argela.com.tr>
2019-03-07 16:54:00 +0300

    First commit

---
+++ src/main/java/com/dogukanku/kafka/listener/KafkaConsumer.java
@@ -0,0 +1,25 @@
+package com.dogukanku.kafka.listener;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.PartitionOffset;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(
+            topicPartitions = @TopicPartition(topic = "voltha.events",
+                    partitionOffsets = {
+                            @PartitionOffset(partition = "0", initialOffset = "0")
+                    }))
+    public void listenToParition(
+            @Payload String message,
+            @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
+        System.out.println(
+                "Received Messasge: " + message + " from partition: " + partition);
+    }
+}
